8ad6f75165dbd526fb2e3e57590c1112120676d5
Kuba <kubablaszczak@gmail.com>
2022-08-06 20:12:01 +0200

    Add vis script

---
+++ plotdata.m
@@ -0,0 +1,28 @@
+elevGPS = coordinates(7,:);
+elevINS = coordinates(4,:);
+latGPS = coordinates(5,:);
+latINS = coordinates(2,:);
+lonGPS = coordinates(6,:);
+lonINS = coordinates(3,:);
+time = coordinates(1,:);
+
+plot(time, lonGPS, time, lonINS);
+legend("GPS", "INS");
+
+hold on;
+
+worldmap 'world';
+[latlim,lonlim] = geoquadline(latGPS,lonGPS);
+buf = 10;
+[latlim,lonlim] = bufgeoquad(latlim,lonlim,buf,buf);
+ax = worldmap(latlim,lonlim);
+oceanColor = [.5 .7 .9];
+setm(ax,'FFaceColor',oceanColor);
+geoshow('landareas.shp','FaceColor',[0.5 0.7 0.5]);
+geoshow('worldrivers.shp','Color', 'blue');
+geoshow('worldcities.shp','Marker','.','Color','red');
+geoshow(latGPS,lonGPS,'Color','y');
+hold on;
+geoshow(latINS,lonINS,'Color','m');
+axis on
+clear latlim lonlim oceanColor lonX lonZ latX latZ buf ax;
